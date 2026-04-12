a00161338d436dbd1ba2317aef2ef08ba23867a3
leader-ke <kenisiaho@gmail.com>
2026-04-12 16:39:17 +0300

    jest: switch config to JS to avoid ts-node dependency in CI

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/unit/**/*.test.ts'],
@@ -9,4 +8,4 @@ const config: Config = {
   },
 };
 
-export default config;
+module.exports = config;
